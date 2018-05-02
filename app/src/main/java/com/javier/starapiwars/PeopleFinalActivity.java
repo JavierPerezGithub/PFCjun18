2d8a22b3b618532a68c4b493b18a7ed731d157c9
javierperez <javierperezjaurena@gmail.com>
2018-05-02 20:30:13 +0200

    He tenido que quitar todo el consumo a la Api e integrar en FireBase toda la BBDD por lo que ahora se podrá consultar los personajes por su nombre.

--- app/src/main/java/com/javier/starapiwars/PeopleFinalActivity.java
+++ app/src/main/java/com/javier/starapiwars/PeopleFinalActivity.java
@@ -1,6 +1,5 @@
 package com.javier.starapiwars;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
