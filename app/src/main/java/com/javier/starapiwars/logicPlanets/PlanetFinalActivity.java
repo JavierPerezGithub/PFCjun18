d51d0ab930da2a7de537646afc70e1235668ef7a
jesus <jesusduro19@hotmail.com>
2018-06-05 21:16:12 +0200

    Aplicacion fianlizada

--- app/src/main/java/com/javier/starapiwars/logicPlanets/PlanetFinalActivity.java
+++ app/src/main/java/com/javier/starapiwars/logicPlanets/PlanetFinalActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,7 +23,8 @@ import com.javier.starapiwars.models.Planet;
 public class PlanetFinalActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private TextView tvNombre, tvRotation, tvOrbital, tvDiameter, tvClimate, tvGravity, tvTerrain, tvSurface, tvPopulation;
+    private TextView tvNombre, tvRotation, tvOrbital, tvDiameter, tvClimate, tvGravity, tvTerrain, tvSurface, tvPopulation,
+            text,text1,text2,text3,text4,text5,text6,text7,text8,text9,text10;
     private Planet planet;
 
     @Override
@@ -31,14 +33,44 @@ public class PlanetFinalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_planet_final);
         imageView = findViewById(R.id.ivPlanetFinal);
         tvNombre = findViewById(R.id.tvNamePlanetFinal);
+        tvNombre.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvRotation = findViewById(R.id.tvRotationPlanetFinal);
+        tvRotation.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvOrbital = findViewById(R.id.tvOrbitalPlanetFinal);
+        tvOrbital.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvDiameter = findViewById(R.id.tvDiameterPlanetFinal);
+        tvDiameter.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvClimate = findViewById(R.id.tvClimatePlanetFinal);
+        tvClimate.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvGravity = findViewById(R.id.tvGravityPlanetFinal);
+        tvGravity.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvTerrain = findViewById(R.id.tvTerrainPlanetFinal);
+        tvTerrain.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvSurface = findViewById(R.id.tvSurfacePlanetFinal);
+        tvSurface.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvPopulation = findViewById(R.id.tvPopulationPlanetFinal);
+        tvPopulation.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+
+        text = findViewById(R.id.textView2);
+        text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text8 = findViewById(R.id.textView3);
+        text8.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text9 = findViewById(R.id.textView4);
+        text9.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text1 = findViewById(R.id.textView5);
+        text1.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text2 = findViewById(R.id.textView6);
+        text2.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text3 = findViewById(R.id.textView5);
+        text3.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text4 = findViewById(R.id.textView7);
+        text4.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text5 = findViewById(R.id.textView8);
+        text5.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text6 = findViewById(R.id.textView9);
+        text6.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text7 = findViewById(R.id.textView10);
+        text7.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
 
         planet = (Planet) getIntent().getSerializableExtra("object");
 
--- app/src/main/java/com/javier/starapiwars/logicStarShips/StarshipFinalActivity.java
+++ app/src/main/java/com/javier/starapiwars/logicStarShips/StarshipFinalActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,7 +24,8 @@ public class StarshipFinalActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private TextView tvNombre, tvModel, tvManufacturer, tvCost, tvLength, tvAthmosphere, tvCrew;
-    private TextView tvPassengers, tvCargoCap, tvConsumables, tvHyperDrive, tvMglt, tvStClass;
+    private TextView tvPassengers, tvCargoCap, tvConsumables, tvHyperDrive, tvMglt, tvStClass
+            , text,text1,text2,text3,text4,text5,text6,text7,text8,text9,text10;
     private Starship starship;
 
     @Override
@@ -52,18 +54,61 @@ public class StarshipFinalActivity extends AppCompatActivity {
         starship = (Starship) getIntent().getSerializableExtra("object");
 
         tvNombre.setText(starship.getName());
+        tvNombre.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvModel.setText(starship.getModel());
+        tvModel.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvManufacturer.setText(starship.getManufacturer());
+        tvManufacturer.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvCost.setText(starship.getCostInCredits());
+        tvCost.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvLength.setText(starship.getLength());
+        tvLength.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvAthmosphere.setText(starship.getMaxAtmospheringSpeed());
+        tvAthmosphere.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvCrew.setText(starship.getCrew());
+        tvCrew.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvPassengers.setText(starship.getPassengers());
+        tvPassengers.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvCargoCap.setText(starship.getCargoCapacity());
+        tvCargoCap.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvConsumables.setText(starship.getConsumables());
+        tvConsumables.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvHyperDrive.setText(starship.getHyperdriveRating());
+        tvHyperDrive.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvMglt.setText(starship.getMglt());
+        tvMglt.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
         tvStClass.setText(starship.getStarshipClass());
+        tvStClass.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+
+
+        text = findViewById(R.id.textView2);
+        text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text8 = findViewById(R.id.textView3);
+        text8.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text9 = findViewById(R.id.textView4);
+        text9.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text1 = findViewById(R.id.textView5);
+        text1.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text2 = findViewById(R.id.textView6);
+        text2.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text3 = findViewById(R.id.textView5);
+        text3.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text4 = findViewById(R.id.textView7);
+        text4.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text5 = findViewById(R.id.textView8);
+        text5.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text6 = findViewById(R.id.textView9);
+        text6.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text7 = findViewById(R.id.textView10);
+        text7.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text7 = findViewById(R.id.textView11);
+        text7.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text7 = findViewById(R.id.textView12);
+        text7.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text7 = findViewById(R.id.textView13);
+        text7.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
+        text7 = findViewById(R.id.textView14);
+        text7.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/starjout.ttf"));
 
         FirebaseStorage storage = FirebaseStorage.getInstance();
         String ref = "fotoStarships/%1s";
